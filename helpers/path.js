5802f85e1cad37a7d60b13f0b6f3cda24de9f5f3
idolago <idolago94@gmail.com>
2020-04-02 20:22:25 +0300

    fix generate path

--- helpers/path.js
+++ helpers/path.js
@@ -1,5 +1,6 @@
 const generateUrl = (path) => {
-    return `http://127.0.0.1:8080/${path}`;
+    // return `http://127.0.0.1:8080/${path}`;
+    return `http://34.69.232.216:8080/${path}`
 }
 
 module.exports = generateUrl;
