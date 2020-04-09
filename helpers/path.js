3246df6209039816faa086e319793cd68b19f420
idolago <idolago94@gmail.com>
2020-04-09 20:57:59 +0300

    generate url(google server)

--- helpers/path.js
+++ helpers/path.js
@@ -1,10 +1,10 @@
 const generateUrl = (type, path) => {
     if(type.includes('video')) {
-        return `http://127.0.0.1:8080/video?p=${path}`; // local url
-        // return `http://34.69.232.216:8080/video?p=${path}` // google server url
+        // return `http://127.0.0.1:8080/video?p=${path}`; // local url
+        return `http://34.69.232.216:8080/video?p=${path}` // google server url
     } else {
-        return `http://127.0.0.1:8080/${path}`; // local url
-        // return `http://34.69.232.216:8080/${path}` // google server url
+        // return `http://127.0.0.1:8080/${path}`; // local url
+        return `http://34.69.232.216:8080/${path}` // google server url
     }
 }
 
