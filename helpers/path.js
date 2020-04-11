f4080c11066dfd8db087d349c56bf6b76c0f31aa
idolago <idolago94@gmail.com>
2020-04-11 21:30:02 +0300

    server url

--- helpers/path.js
+++ helpers/path.js
@@ -1,10 +1,11 @@
+// const base_url = `http://127.0.0.1:8080`; // local url
+const base_url = `http://34.69.232.216:8080` // google server url
+
 const generateUrl = (type, path) => {
     if(type.includes('video')) {
-        // return `http://127.0.0.1:8080/video?p=${path}`; // local url
-        return `http://34.69.232.216:8080/video?p=${path}` // google server url
+        return `${base_url}/video?p=${path}`;
     } else {
-        // return `http://127.0.0.1:8080/${path}`; // local url
-        return `http://34.69.232.216:8080/${path}` // google server url
+        return `${base_url}/${path}`;
     }
 }
 
