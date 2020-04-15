edf146b49cde0e9c853aa1ae91fd1ee2d8625234
idolago <idolago94@gmail.com>
2020-04-15 17:50:28 +0300

    0.5

--- middleware/upload.js
+++ middleware/upload.js
@@ -91,9 +91,9 @@ const storageVideo = (req, res, next) => {
   let videoFile = req.file;
   // videoFile - return from multer (fieldname, originalname, encoding, mimetype, destination, filename, path, size)
   // let outputPath = `${__dirname}/../files/${req.query.id}/${req.query.secret ? ('secrets/'):('uploads/')}${videoFile.filename}`;
-  let outputPath = path.join(__dirname, 'files', req.query.id, req.query.secret ? ('secrets/'):('uploads/'), videoFile.filename);
+  let outputPath = path.join(__dirname, '../files', req.query.id, req.query.secret ? ('secrets/'):('uploads/'), videoFile.filename);
   // ffmpeg(`${__dirname}/../${videoFile.path}`)
-  ffmpeg(path.join(__dirname, videoFile.path))
+  ffmpeg(path.join(__dirname, '..', videoFile.path))
     .inputOptions(['-vcodec h264', '-acodec aac'])
     .on("start", commandLine => {
       console.log(`Spawned FFmpeg with command: ${commandLine}`);
