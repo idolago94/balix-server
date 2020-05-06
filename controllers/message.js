8c17a8a08eb4798a18b39831fc8e7f327d5a35f1
idolago <idolago94@gmail.com>
2020-05-06 19:09:36 +0300

    chat with socket

--- controllers/message.js
+++ controllers/message.js
@@ -26,7 +26,28 @@ const sendMessage = async(req, res) => {
     res.json(sendResponse);
 }
 
+const sendMessageBySocket = (message_data) => {
+    return new Promise(async(resolve, reject) => {
+        let room_id = message_data.room_id;
+        let user_id = message_data.user_id;
+        let context = message_data.context;
+        let roomData = null;
+        if(room_id != '') {
+            roomData = await chatRoomMiddleware.getRoomById(room_id);
+        }
+    
+        if(!roomData) {
+            let otherUser = message_data.receive_user;
+            roomData = await chatRoomMiddleware.createRoom([otherUser, user_id]);
+            room_id = roomData._id;
+        }
+        let sendResponse = await messageMiddleware.addMessage(room_id, user_id, context);
+        resolve(sendResponse);
+    })
+}
+
 module.exports = {
     getRoomMessages,
-    sendMessage
+    sendMessage,
+    sendMessageBySocket
 }
