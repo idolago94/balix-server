9591d84ff30bb444e52d63a114c0ee0d069f375d
idolago <idolago94@gmail.com>
2020-05-07 13:37:19 +0300

    update visit route

--- routes/chatRoom.js
+++ routes/chatRoom.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const tokenMiddleware = require('../middleware/token');
 const chatRoomMiddleware = require('../middleware/chatRoom');
+const userMiddleware = require('../middleware/users');
 
 router.get('/', tokenMiddleware.verify, async(req, res, next) => {
     let user_id = req.headers.user;
@@ -27,5 +28,17 @@ router.delete('/delete', tokenMiddleware.adminVerify, async(req, res, next) => {
     let response = await chatRoomMiddleware.deleteRoom(room_id);
     res.json(response);
 });
+
+router.put('/visit', tokenMiddleware.verify, async(req, res) => {
+    let room_id = req.query.id;
+    let user_id = req.headers.user;
+
+    let user = await userMiddleware.getUser(user_id);
+    let rooms_obj = user.chat_rooms;
+    rooms_obj[room_id] = new Date();
+
+    let response = await userMiddleware.updateUser(user_id, {chat_rooms: rooms_obj});
+    res.json({chat_rooms: response.chat_rooms});
+})
 
 module.exports = router;
